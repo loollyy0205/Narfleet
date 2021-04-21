69fd2d8de38342546dd7aac208c5942fbae9301a
Francis Saavedra <msaavedra.k11504970@umak.edu.ph>
2021-04-21 11:58:16 +0800

    about page done responsive 1366

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,6 +1,7 @@
 import AboutBanner from "./about-banner/AboutBanner"
 import AboutContent from "./about-content/AboutContent"
 import AboutSearch from "./about-search/AboutSearch"
+import ExploreNext from "./explore-next/ExploreNext"
 
 const About = () => {
     return (
@@ -8,6 +9,7 @@ const About = () => {
             <AboutBanner />
             <AboutSearch />
             <AboutContent />
+            <ExploreNext />
         </div>
     )
 }
